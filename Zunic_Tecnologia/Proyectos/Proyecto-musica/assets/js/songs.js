6d89e04446ab030a6eaaee4101b590554ac85f23
ZunicTecnologia <ZunicTecnologia@gmail.com>
2025-02-20 12:45:25 -0400

    Add files via upload

---
+++ Zunic_Tecnologia/Proyectos/Proyecto-musica/assets/js/songs.js
@@ -0,0 +1,50 @@
+const sampleSongs = [
+  {
+    title: 'Emocionante-Música-Instrumental-Piano',
+    artist: 'nose',
+    album: 'Hurry Up, We\'re Dreaming',
+    cover: 'assets/img/musica_1.PNG',
+    url: 'assets/music/Emocionante-Música-Instrumental-Piano.mp3',
+    duration: 255
+  },
+  {
+    title: 'COBRAs',
+    artist: 'The Midnight',
+    album: 'Days of Thunder',
+    cover: 'assets/img/musica_2.PNG',
+    url: 'assets/music/COBRA.mp3',
+    duration: 198
+  },
+  {
+    title: 'Calm-Wind',
+    artist: 'GUNSHIP',
+    album: 'Dark All Day',
+    cover: 'assets/img/musica_3.PNG',
+    url: 'assets/music/Calm-Wind.mp3',
+    duration: 274
+  },
+  {
+    title: 'Neon Lights',
+    artist: 'FM-84',
+    album: 'Atlas',
+    cover: 'https://images.unsplash.com/photo-1614613535829-3f89a4dd9290?w=400&q=80',
+    url: 'https://example.com/song4.mp3',
+    duration: 312
+  },
+  {
+    title: 'Digital Love',
+    artist: 'Daft Punk',
+    album: 'Discovery',
+    cover: 'https://images.unsplash.com/photo-1614613534010-74ae475f8c58?w=400&q=80',
+    url: 'https://example.com/song5.mp3',
+    duration: 301
+  },
+  {
+    title: 'Sunset Drive',
+    artist: 'TimeCop1983',
+    album: 'Night Drive',
+    cover: 'https://images.unsplash.com/photo-1614613534258-3f89a4dd9290?w=400&q=80',
+    url: 'https://example.com/song6.mp3',
+    duration: 285
+  }
+];
